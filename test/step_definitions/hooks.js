55de7e8335a1201766754416d3d559df71e9ba40
Yuriy Berdnik <yuriy8671@gmail.com>
2020-04-29 18:50:50 +0300

    Behavior-Driven Development (BDD) in TA

--- test/step_definitions/hooks.js
+++ test/step_definitions/hooks.js
@@ -1,12 +1,12 @@
-
 const { After, Status, setDefaultTimeout } = require('cucumber');
 
 setDefaultTimeout(60 * 1000);
 
-After((testCase) => {
+After(function (testCase) {
 	if (testCase.result.status === Status.FAILED) {
 		return browser.takeScreenshot().then((screenShot) => {
-			return this.attach(Buffer.from(screenShot, 'base64'), 'image/png');
+			const decodedImage = Buffer.from(screenShot, 'base64');
+			return this.attach(decodedImage, 'image/png');
 		});
 	}
 });
